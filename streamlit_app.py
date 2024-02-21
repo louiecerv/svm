1f175976a99d033a996cbfa9208482e233ecb10c
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-02-21 16:07:31 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ def app():
     st.subheader('Description')
 
     text = """
-        Replace with description of SVM. """)
-
+        Replace with description of SVM. """
+    st.write(text)
     if st.button('Start'):
         X, y = make_blobs(n_samples=200, centers=2, center_box=(-10, 10), cluster_std=1.0, random_state=42)     
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3)
