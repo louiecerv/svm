149425d611e92cbc0df5c890b7f1f392dd88d21d
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-02-22 02:25:42 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,6 +71,10 @@ def app():
     
             # Scatter plot of the data
             ax.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
+            
+            # Plot the decision function directly on ax
+            xlim = ax.get_xlim()
+            ylim = ax.get_ylim()
 
             xx = np.linspace(xlim[0], xlim[1], 30)
             yy = np.linspace(ylim[0], ylim[1], 30)
